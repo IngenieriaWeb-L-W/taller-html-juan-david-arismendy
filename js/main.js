17fdcc1d3bce0cab40cc516426bdc7c59ff6d9d0
juan-arismendy <juan.arismendy@udea.edu.co>
2024-02-24 17:46:05 -0500

    Add comments explaining the code

--- js/main.js
+++ js/main.js
@@ -1,20 +1,30 @@
+// Function to insert a number into the calculator display
 function insert(num) {
+    // Append the number to the current display value
     document.form.textview.value = document.form.textview.value + num;
 }
 
+// Function to calculate the result of the expression in the display
 function equal() {
+    // Retrieve the current expression from the display
     let exp = document.form.textview.value;
 
+    // If the expression is not empty, evaluate it and display the result
     if (exp) {
         document.form.textview.value = eval(exp);
     }
 }
 
+// Function to clear the calculator display
 function clean() {
+    // Set the display value to an empty string
     document.form.textview.value = '';
 }
 
+// Function to remove the last character from the calculator display
 function back() {
+    // Retrieve the current expression from the display
     let exp = document.form.textview.value;
-    document.form.textview.value = exp.substring(0, exp.length - 1);
+    // Remove the last character from the expression and update the display
+    document.form.textview.value = exp.substring(0, exp.length -  1);
 }
