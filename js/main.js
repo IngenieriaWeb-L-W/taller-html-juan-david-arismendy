8456a3ae262e0d897938ef3e0eb0901af6e3e6d5
juan-arismendy <juan.arismendy@udea.edu.co>
2024-02-25 11:02:34 -0500

    Add special operations ( mod,exp,&)

--- js/main.js
+++ js/main.js
@@ -1,13 +1,18 @@
 // Function to insert a number into the calculator display
 function insert(num) {
     // Append the number to the current display value
-    document.form.textview.value = document.form.textview.value + num;
+    if (num == '**') {
+        document.form.textview.value = document.form.textview.value + '^';
+    }
+    else
+        document.form.textview.value = document.form.textview.value + num;
 }
 
 // Function to calculate the result of the expression in the display
 function equal() {
     // Retrieve the current expression from the display
     let exp = document.form.textview.value;
+    exp = exp.replace("^", "**")
 
     // If the expression is not empty, evaluate it and display the result
     if (exp) {
@@ -26,5 +31,5 @@ function back() {
     // Retrieve the current expression from the display
     let exp = document.form.textview.value;
     // Remove the last character from the expression and update the display
-    document.form.textview.value = exp.substring(0, exp.length -  1);
+    document.form.textview.value = exp.substring(0, exp.length - 1);
 }
